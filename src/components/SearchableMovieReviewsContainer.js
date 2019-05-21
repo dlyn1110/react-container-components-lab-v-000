39b868d97d0cee1039f98a9ca0d3dc093f0c5b9e
Dani Lyn <dlyn1110@gmail.com>
2019-05-21 18:52:06 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,23 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+
+   constructor(){
+     super()
+
+     this.state = {
+       searchTerm: '',
+       reviews: []
+     }
+   }
+
+   handleSearchMovieInput = event => this.setState({ searchTerm: event.target.value });
+
+   handleSubmit = event => {
+     event.preventDefault();
+
+     fetch(SEARCH_URL.concat(this.state.searchTerm))
+     .then(res => res.json())
+     .then(res => this.setState({ reviews: res.results }))
+   }
