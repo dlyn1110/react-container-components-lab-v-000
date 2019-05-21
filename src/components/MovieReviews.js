7b7d462b45d8385c77821261ac0f078df79fda00
Dani Lyn <dlyn1110@gmail.com>
2019-05-21 18:42:56 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,15 +3,15 @@ import React from 'react';
 
 const review = ({ display_title, headline, summary_short }) => {
 
-  return(
-    <div classname="review">
-    <h2>{ display_title }</h2>
-    <h3>{ headline }</h3>
-    <p>{ summary_short }</p>
-    </div>
-  )
+    return(
+        <div className="review">
+            <h2>{ display_title }</h2>
+            <h3>{ headline }</h3>
+            <p>{ summary_short }</p>
+        </div>
+    )
 }
 
- const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(review)}</div>
+const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(review)}</div>
 
- export default MovieReviews
+export default MovieReviews
