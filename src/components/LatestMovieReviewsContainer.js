af187c5254025b8bb94a8946dee250d5aff2a774
Dani Lyn <dlyn1110@gmail.com>
2019-05-21 18:19:12 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -8,6 +8,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 
 // Code LatestMovieReviewsContainer Here
 class LatestMovieReviewsContainer extends Component {
+  
   constructor() {
     super()
     this.state = {
